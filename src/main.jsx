6cb3a4d4af54f007e23b5eca29a333bf54154b7a
praful-devv <your_github_email@example.com>
2026-08-25 20:37:14 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import {BrowserRouter} from 'react-router-dom'
-import SidebarContext from "./context/sidebarContext.jsx";
+import SidebarContext from "./context/SidebarContext.jsx";
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import RecipesCOntext from "./context/RecipesCOntext.jsx";
